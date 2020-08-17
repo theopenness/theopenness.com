1efad1c3b5504b544ddf8288f4467b9e8a568c54
notoliver <47568829+notoliver@users.noreply.github.com>
2020-08-17 16:06:16 -0400

    h

--- init.js
+++ init.js
@@ -23,3 +23,12 @@ $(document).ready(function(){
         centerPadding: 10
     });
 });
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
