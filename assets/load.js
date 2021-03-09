43467960bc1f283d5bdf168260715d0025d58f09
notoliver <47568829+notoliver@users.noreply.github.com>
2021-03-08 23:39:12 -0500

    lil dynamic action

---
+++ assets/load.js
@@ -0,0 +1,46 @@
+var videotext = '';
+/*
+<div class='vid'>
+    <div class='container'>
+        <iframe class='responsive-iframe' src='https://www.youtube.com/embed/Dcm6nnO5FW4?controls=0' frameborder='0' allowfullscreen></iframe>
+    </div>
+</div>
+*/
+for(var i=0;i<videos.length;i++){
+    videotext += '<div class=\'vid\'>\r\n\t\t\t\t<div class=\'container\'>\r\n\t\t\t\t\t<iframe class=\'responsive-iframe\' src=\''
+        +videos[i]+'\' frameborder=\'0\' allowfullscreen><\/iframe>\r\n\t\t\t\t<\/div>\r\n\t\t\t<\/div>';
+}
+
+document.getElementById('videoreleases').innerHTML = videotext;
+
+var musictext = '';
+/*
+<a href='/journey/' target='_blank'><div class='rcrd'><img src='assets/journey.jpg'/><h1>Adore the Journey</h1></div></a>
+*/
+for(var i=0;i<music.length;i++){
+    musictext += '<a href=\''
+        +music[i].link+'\' target=\'_blank\'><div class=\'rcrd\'><img src=\'..\/assets\/'
+        +music[i].image+'\'\/><h1>'+music[i].name+'<\/h1><\/div><\/a>';
+}
+
+document.getElementById('musicreleases').innerHTML = musictext;
+
+var collabtext ='';
+/*
+<a href='https://open.spotify.com/artist/3PqrDJmnmIbb2KJV70SYKH?si=tbBFGDtJQh6IasKV58Vt6w' target='_blank'>Joseph Figueroa</a><br>
+Alex Kah<br>
+*/
+for(var i=0;i<collaborators.length;i++){
+    var c = collaborators[i];
+    if(c.link=='none'){
+        collabtext+=c.name;
+    }
+    else{
+        collabtext+='<a href=\''+c.link+'\' target=\'_blank\'>'+c.name+'<\/a>'
+    }
+    if(i!=collaborators.length-1){
+        collabtext+="<br>"
+    }
+}
+
+document.getElementById('textnames').innerHTML = collabtext;
---
+++ entries/data.js
@@ -0,0 +1,165 @@
+var videos = [
+    "https://www.youtube.com/embed/232W4kYZTSg?controls=0",
+    "https://www.youtube.com/embed/0UK7qFX6wfU?controls=0",
+    "https://www.youtube.com/embed/Dcm6nnO5FW4?controls=0",
+    "https://www.youtube.com/embed/7Lxm3ylOiUI?controls=0",
+    "https://www.youtube.com/embed/gk1YazNzs1U?controls=0",
+    "https://www.youtube.com/embed/OH41aiE7EyY?controls=0",
+    "https://www.youtube.com/embed/OUogMTJ8yaM?controls=0",
+    "https://www.youtube.com/embed/jJkfoUpdFoE?controls=0",
+    "https://www.youtube.com/embed/aseuyRHniow?controls=0",
+    "https://www.youtube.com/embed/33rpVvb1GZU?controls=0",
+    "https://www.youtube.com/embed/AqsTk0mkafA?controls=0"
+];
+
+var music = [
+    {
+        link:"/journey/",
+        image:"journey.jpg",
+        name:"Adore the Journey"
+    },
+    {
+        link:"https://distrokid.com/hyperfollow/openness/tunnel-vision",
+        image:"tunnelvision.jpg",
+        name:"Tunnel Vision"
+    },
+    {
+        link:"/tokyo/",
+        image:"lastsummerintokyo.jpg",
+        name:"Last Summer in Tokyo"
+    },
+    {
+        link:"https://openness.bandcamp.com/album/songs-from-the-open-nest",
+        image:"opennest.jpg",
+        name:"Songs from the Open Nest"
+    },
+    {
+        link:"https://openness.bandcamp.com/album/artistree",
+        image:"artistree.jpg",
+        name:"Artistree"
+    },
+    {
+        link:"https://openness.bandcamp.com/album/blossom",
+        image:"blossom.jpg",
+        name:"Blossom"
+    }
+];
+
+var collaborators = [
+    {
+        link:"https://oliverqiu.com/",
+        name:"Oliver Qiu"
+    },
+    {
+        link:"https://open.spotify.com/artist/0V4dkDwF4H24OP6HAyzZT2?si=P0taIrn9ScWL5iswZoIvKQ",
+        name:"Kyle Loeffler"
+    },
+    {
+        link:"https://open.spotify.com/artist/3mnfCPeV6fz1xyCcLCE1W2?si=T6DSbmikSpe8xlAfbqpBRw",
+        name:"Ronan Dowling (Ronan Andrews)"
+    },
+    {
+        link:"https://rosemarysprig.bandcamp.com/",
+        name:"Hannah Stomski (Rosemary)"
+    },
+    {
+        link:"https://j-mo.bandcamp.com/",
+        name:"Jaemeraquan David (Jaemeraquan)"
+    },
+    {
+        link:"https://open.spotify.com/artist/2xtFxo10GnCNUDGZbykA7P?si=_Y0T7QMaQFG8-hSlaO-x1A",
+        name:"Julia Stoermer (Jooly)"
+    },
+    {
+        link:"https://thebeachpeople.bandcamp.com/",
+        name:"Kevin Cincotta"
+    },
+    {
+        link:"https://open.spotify.com/artist/6lTmUkd2VieovYFr6QBcFD?si=T-FdwyDBTEeMuVSrHjq2eA",
+        name:"Brodie Eastridge"
+    },
+    {
+        link:"https://open.spotify.com/artist/3PqrDJmnmIbb2KJV70SYKH?si=tbBFGDtJQh6IasKV58Vt6w",
+        name:"Joseph Figueroa"
+    },
+    {
+        link:"none",
+        name:"Alex Kah"
+    },
+    {
+        link:"https://open.spotify.com/artist/51v3qPD0rpxSN8pHUWO9ZO?si=DDljqbKwQv-katMvKauOLw",
+        name:"Kendon McGehee (Tarantula Bill)"
+    },
+    {
+        link:"https://open.spotify.com/artist/5w3XZFFZtefQtBYM0liqEW?si=wwwFlrCfTrKvjQDeaZifyw",
+        name:"Dante Robledo (Don Scrawn)"
+    },
+    {
+        link:"https://open.spotify.com/artist/0ob3DBheTSlGC666UtNmIY?si=pJ-EObFFQhKYjGmrrLamrw",
+        name:"Josh Van Wey"
+    },
+    {
+        link:"ttps://open.spotify.com/artist/021Xr8IrEYBBXrM1VJaJaZ?si=AUEFlIb8RwK8e-RhZcn1jw",
+        name:"Lauren Tilton (ii/lo)"
+    },
+    {
+        link:"https://open.spotify.com/artist/2rRKwSbT80Ss7cfcz8FeAG?si=xvuyT12JSbqHM0qvew2WhQ",
+        name:"Julian Strütt (SENTO)"
+    },
+    {
+        link:"https://thebeachpeople.bandcamp.com/",
+        name:"Bridget Hartman"
+    },
+    {
+        link:"https://www.facebook.com/fauxmantramusic/",
+        name:"Julia Walters"
+    },
+    {
+        link:"none",
+        name:"Emilia Richte"
+    },
+    {
+        link:"https://www.instagram.com/mirandaraehey/",
+        name:"Miranda Junasa"
+    },
+    {
+        link:"https://dreampool.bandcamp.com/releases",
+        name:"Riley Merino"
+    },
+    {
+        link:"https://open.spotify.com/artist/0hFZ0YjjAxZwn2kbTTIGv2?si=PuX-xbQcRqSh6ekWygFNow",
+        name:"April Levin (April Gloom)"
+    },
+    {
+        link:"https://camelmile.bandcamp.com/releases",
+        name:"Cash Globe"
+    },
+    {
+        link:"https://afterthecarnival.bandcamp.com/",
+        name:"Julia Hoffman"
+    },
+    {
+        link:"https://upstandingcitizen.bandcamp.com/",
+        name:"Cole Eckert"
+    },
+    {
+        link:"https://upstandingcitizen.bandcamp.com/",
+        name:"Sam Waugaman"
+    },
+    {
+        link:"none",
+        name:"Alia Norwood"
+    },
+    {
+        link:"https://open.spotify.com/artist/558ImT4Z24CcO5QnH4gxZy?si=JWFbaPBASxOB8OyTdrWFlw",
+        name:"Chris Lee (Scooterraps)"
+    },
+    {
+        link:"https://www.instagram.com/whitetrashwilly/",
+        name:"Will Cashion"
+    },
+    {
+        link:"https://benparrish.bandcamp.com/",
+        name:"Ben Parrish"
+    }
+];
